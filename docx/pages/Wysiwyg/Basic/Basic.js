559afa48309e53b395a3bd6ab0b1ba791aadc1c4
justinlazaro-ubidy <justin.lazaro@gmail.com>
2018-01-31 17:49:21 +0800

    lib to component wysiwyg

--- docx/pages/Wysiwyg/Basic/Basic.js
+++ docx/pages/Wysiwyg/Basic/Basic.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Wysiwyg from '../../../../lib/Wysiwyg';
-// import Wysiwyg from 'COMPONENT/Wysiwyg';
+// import Wysiwyg from '../../../../lib/Wysiwyg';
+import Wysiwyg from 'COMPONENT/Wysiwyg';
 
 const Basic = props => {
   return (
